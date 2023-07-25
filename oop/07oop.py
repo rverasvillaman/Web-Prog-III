fb4f4b2839939a35d7782c395d13a0cdbba79a99
Roberto <roberto.verasvillaman@student.fhws.de>
2023-07-25 16:40:48 +0200

    update of past exercises

---
+++ oop/07oop.py
@@ -0,0 +1,16 @@
+class Student:
+    student1 = {
+        "firstname": "Roberto",
+        "lastname": "Veras-Villaman",
+        "degree": "E-Commerce"
+    }
+
+class Administrator:
+    ListOfStudents = []
+    def addStudent(self):
+        ListOfStudents.append(Student)
+
+    def showStudents(self):
+        print(ListOfStudents)
+
+
